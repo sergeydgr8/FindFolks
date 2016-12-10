07fb380a4001b119d3fd8b562a19ac1627d5c110
Sergey Smirnov <sergey@smirnov.nyc>
2016-12-10 18:28:22 -0500

    Initial creation

---
+++ FindFolks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FindFolks.Startup))]
+namespace FindFolks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
